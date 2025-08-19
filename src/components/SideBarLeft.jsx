bfb842b7c18a9532653fdbcd8608ca06d5dcf0f6
Adolfo Landaeta <adolfoland@gmail.com>
2025-08-19 17:40:06 -0400

    Estructura base de una pagina web

---
+++ src/components/SideBarLeft.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const SideBarLeft = () => {
+  return (
+    <aside>
+      <div>SideBarLeft</div>
+    </aside>
+  );
+};
---
+++ src/components/SideBarRight.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const SideBarRight = () => {
+  return (
+    <aside>
+      <div>SideBarLeft</div>
+    </aside>
+  );
+};
